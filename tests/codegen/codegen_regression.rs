6507fd22dedb11e60a237c0f53c0885bb4bc2e06
Ayrton Chilibeck <aa@chilibeck.com>
2024-06-28 05:47:40 -0600

    Moar tests

--- tests/codegen/codegen_regression.rs
+++ tests/codegen/codegen_regression.rs
@@ -19,10 +19,51 @@ mod tests {
     #[test]
     #[cfg(feature = "codegen")]
     fn test_nested_config() {
-        eprintln!("Running codegen tests");
         let mut evaluator = pkl_rs::api::evaluator::Evaluator::new().expect("failed to initialize evaluator");
         let pkl_mod = evaluator.evaluate_module(
             PathBuf::from("tests/codegen/pkl_files/local/nesting.pkl")).expect("failed to evaluate");
+
+        pkl_mod.codegen().expect("failed to codegen");
+    }
+
+    #[test]
+    #[cfg(feature = "codegen")]
+    fn test_primitives() {
+        let mut evaluator = pkl_rs::api::evaluator::Evaluator::new().expect("failed to initialize evaluator");
+        let pkl_mod = evaluator.evaluate_module(
+            PathBuf::from("tests/codegen/pkl_files/local/primitives.pkl")).expect("failed to evaluate");
+
+        pkl_mod.codegen().expect("failed to codegen");
+
+    }
+
+    #[test]
+    #[cfg(feature = "codegen")]
+    fn test_map() {
+        let mut evaluator = pkl_rs::api::evaluator::Evaluator::new().expect("failed to initialize evaluator");
+        let pkl_mod = evaluator.evaluate_module(
+            PathBuf::from("tests/codegen/pkl_files/local/map.pkl")).expect("failed to evaluate");
+
+        pkl_mod.codegen().expect("failed to codegen");
+
+    }
+
+    #[test]
+    #[cfg(feature = "codegen")]
+    fn test_set() {
+        let mut evaluator = pkl_rs::api::evaluator::Evaluator::new().expect("failed to initialize evaluator");
+        let pkl_mod = evaluator.evaluate_module(
+            PathBuf::from("tests/codegen/pkl_files/local/set.pkl")).expect("failed to evaluate");
+
+        pkl_mod.codegen().expect("failed to codegen");
+    }
+
+    #[test]
+    #[cfg(feature = "codegen")]
+    fn test_list() {
+        let mut evaluator = pkl_rs::api::evaluator::Evaluator::new().expect("failed to initialize evaluator");
+        let pkl_mod = evaluator.evaluate_module(
+            PathBuf::from("tests/codegen/pkl_files/local/list.pkl")).expect("failed to evaluate");
 
         pkl_mod.codegen().expect("failed to codegen");
     }
